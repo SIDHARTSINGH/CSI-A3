b7fd67d0d151e14e4c9ece835d83a3eaef660ea9
Sidhart Singh <isidhartsingh@gmail.com>
2023-07-20 07:32:02 +0530

    Style Course component

--- src/components/Course.tsx
+++ src/components/Course.tsx
@@ -1,7 +1,23 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Divider, Flex, Heading, Stack, Text } from "@chakra-ui/react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Course = () => {
-  return <Heading>Course</Heading>;
+  return (
+    <Flex direction="column" minHeight="100vh">
+      <Navbar />
+      <Stack flexGrow={1} p={10} align={"center"}>
+        <Heading as={"h1"}>Courses</Heading>
+        <Divider />
+        <Text fontSize={"xl"} p={10}>
+          Coming Soon
+        </Text>
+      </Stack>
+      <Box as="footer" flexShrink={0}>
+        <Footer />
+      </Box>
+    </Flex>
+  );
 };
 
 export default Course;
